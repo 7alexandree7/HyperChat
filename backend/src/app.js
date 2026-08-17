da7abab5bd52d60f95921e82334983710e111d62
alexandre <alexandre020602@gmail.com>
2026-08-16 21:52:15 -0300

    feat: add middleware for JSON parsing, CORS, and Clerk authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { ENV_VARIABLES } from "./config/ENV_VARIABLES.js";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
+app.use(express.json());
+app.use(cors({ origin: ENV_VARIABLES.ORIGIN, credentials: true }));
+app.use(clerkMiddleware())
 
 export default app
