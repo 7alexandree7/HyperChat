ea6396cff90234d80abc7c971f684d5dae3b7361
alexandre <alexandre020602@gmail.com>
2026-08-19 20:47:32 -0300

    refactor: replace cron import with job from lib and adjust formatting

--- backend/src/db/ConnectDB.js
+++ backend/src/db/ConnectDB.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import cron from "node-cron";
 import { ENV_VARIABLES } from "../config/ENV_VARIABLES.js";
+import job from "../lib/cron.js";
 
 
 export async function ConnectDB() {
     try {
         await mongoose.connect(ENV_VARIABLES.DB_URL);
         console.log("Connected to MongoDB");
-        if (ENV_VARIABLES.NODE_ENV === "production") job.start();
+        if (ENV_VARIABLES.NODE_ENV === "production")  job.start();
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
         process.exit(1);
