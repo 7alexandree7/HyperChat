39f3586bec3a20718181e4771496ac71d8a8b841
alexandre <alexandre020602@gmail.com>
2026-08-30 13:42:05 -0300

    fix: correct routing logic for authentication and chat pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,11 @@ function App() {
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
-          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace/>} />
-          <Route path="/auth" element={ <AuthPage />} />
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
         </Routes>
       </WallpaperProvider>
     </ThemeProvider>
