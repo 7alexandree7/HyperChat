82976452f9d92afa03d3859fd79e1f34ddaef664
alexandre <alexandre020602@gmail.com>
2026-08-30 13:57:03 -0300

    feat: update API base path to v1 and add axios instance for API requests

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,7 +4,7 @@ import messageRoute from "./message.route.js";
 
 const router = Router();
 
-const BASE_PATH = "/api/v1";
+const BASE_PATH = "/api";
 
 router.use(`${BASE_PATH}/auth`, authRoute);
 router.use(`${BASE_PATH}/messages`, messageRoute)
